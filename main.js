799b8fb218d1077153811fa5a3cf241e24260294
M0H1K4N <nejer911@gmail.com>
2024-02-20 20:05:38 +0400

    Add

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 /* Write a function createHelloWorld. It should return a new function that always returns "Hello World".*/
-function consoleHelloWorld() {
-        return "Hello World"
-    }
+// function consoleHelloWorld() {
+//         return "Hello World"
+//     }
 
-const d = consoleHelloWorld()
-console.log(d)
+// const d = consoleHelloWorld()
+// console.log(d)
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+const squeredNumbers = numbers.map(num => num * num)
+console.log(squeredNumbers)
